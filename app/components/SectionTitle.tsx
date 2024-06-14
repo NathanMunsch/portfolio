29d79af7c026d8d0f5db7b9ef744a4b830985469
nathanmunschpro <nathanmunschpro@gmail.com>
2024-06-14 19:42:40 +0000

    fix: Fix errors when building

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -1,8 +1,8 @@
-export default function SectionTitle({title}: {title: string}) {
+export default function SectionTitle({ title }: { title: string }) {
     return (
         <h2 className="text-4xl font-bold">
             <span className="mr-2 text-mainBlue">
-                //
+                &#47;&#47;
             </span>
             {title}
         </h2>
